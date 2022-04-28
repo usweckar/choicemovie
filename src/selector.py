07bc7556f799916a6fff04a8cccba839c964454e
usweckar <104538999+usweckar@users.noreply.github.com>
2022-04-29 04:29:13 +0900

    全てを対象にしてランダムに1つ選択する機能を実装 (#8)

---
+++ src/selector.py
@@ -0,0 +1,43 @@
+import json
+import random
+
+import settings
+
+
+class Selector:
+
+    def __init__(
+        self,
+        json_path=settings.RESOURCE_JSON_PATH,
+    ):
+
+        self._json_path = json_path
+
+    def select(self):
+        with open(self._json_path, 'r') as f:
+            raw_data = f.read()
+
+        data = json.loads(raw_data)
+        picked = random.choice(data['movies'])
+
+        url = picked['url']
+        title = picked['title']
+        yyyymmdd = picked['yyyymmdd']
+
+        s = (
+            '\n'
+            '===============================================\n'
+            '動画はこれだよっ！スーパーサンクスしよっ！！\n'
+            '-----------------------------------------------\n'
+            f'{title}'
+            f' ({yyyymmdd})\n'
+            f'{url}\n'
+            '===============================================\n'
+        )
+        return s
+
+
+if __name__ == '__main__':
+    cls = Selector()
+    result = cls.select()
+    print(result)
